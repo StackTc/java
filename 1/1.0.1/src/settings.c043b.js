108253a71a8c7404fc97c79b70b9422e8984b3a7
root <root@ip-172-31-9-65.sa-east-1.compute.internal>
2023-10-27 14:38:52 +0000

    version update1_1.0.1

---
+++ 1/1.0.1/src/settings.c043b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/stage.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"3fbfa",main:"5209d"}};
